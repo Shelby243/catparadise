51f20b253a0607f1b0f8cb172e1a7c584ae3d7c4
Shelby243 <padreshelby@gmail.com>
2022-08-26 13:05:55 +0300

    second commit

--- src/components/Cat.js
+++ src/components/Cat.js
@@ -1,16 +1,17 @@
 const Cat=({
-    cat:{name,origin,life_span,description,weight,temperament}
+    cat:{name,origin,life_span,description,weight,temperament,image}
 })=>{
     
     console.log(description)
     console.log(weight.metric)
     console.log(temperament)
+    console.log(image.url)
     
     return(
         <div className='user-card1'>
-           
+           <img src={image.url} alt={name}/>
             <h1>{name}</h1>
-            <h2>{origin}</h2>
+            <h2>{origin}</h2> 
             <p>{temperament}</p>
             <h3>{life_span}</h3>
             <h4>{weight.metric}</h4>
